2be91738d16638360f110b95f85444c328ad9e97
Camilla Barbieri <camilla.barbieri@accenture.com>
2022-08-01 18:01:28 +0200

    code's clean

--- app.js
+++ app.js
@@ -6,46 +6,9 @@ const toggleChecked = (e, newTask) => {
     itemBtn.innerHTML = `❗️ Done`;
   } else {
     parent.classList.add("checked");
-    // let inputText = document.querySelector(".myText");
-    // p è undefined
     itemBtn.innerHTML = `✔️ Restore`;
-    // let itemText = document.querySelector(".text-item");
-    // itemText = inputText.value;
-
-    // manca toggle al button
-    // let restoreBtn = document.querySelector("button.done");
-    // console.log(restoreBtn);
-
-    // const itemBtn = e.currentTarget;
-    // console.log(e.currentTarget);
-    // itemBtn.addEventListener("click", () => {
-    //   if (itemBtn.classList.contains("checked")) {
-    //     parent.classList.remove("checked");
-    //   } else {
-    //     parent.classList.add("checked");
-    //     console.log("add");
-    //   }
-    // });
   }
 };
-
-// e.currentTarget.addEventListener("click", (e) =>
-//   e.currentTarget.parentNode.classList.contains("checked")
-//     ? e.currentTarget.classList.remove("checked")
-//     : e.currentTarget.classList.add("checked")
-// );
-// console.log(e.currentTarget);
-
-// if (e.currentTarget.classList.contains("checked")) {
-//   const deleteTask = document.querySelectorAll("button.delete");
-//   for (let i = 0; i < deleteTask.length; i++) {
-//     deleteTask[i].addEventListener("click", function () {
-//       this.parentNode.remove();
-//     });
-//   }
-// } else {
-//   e.currentTarget.classList.add("checked");
-// }
 
 const clickAdd = () => {
   let inputText = document.querySelector(".myText");
@@ -74,46 +37,6 @@ const addTasks = () => {
   });
   btnAdd.addEventListener("click", clickAdd);
 };
-
-// const closeBtn = (e) => {
-//   if (e.currentTarget.classList.contains("checked")) {
-//     // checked => devo cancellare la task
-//     // const allTasks = [...document.querySelectorAll("li")];
-//     const newDoneBtn = [...document.querySelectorAll(".delete")];
-//     // allTasks.forEach((el) => el.setAttribute("class", "hidden"));
-//     // allTasks.forEach((el) => e.parentNode.remove());
-//     newDoneBtn.forEach(
-//       (el) =>
-//         function () {
-//           this.parentNode.remove();
-//         }
-//     );
-//   } else {
-//     e.currentTarget.classList.add("checked");
-//   }
-// };
-
-// const toggleItems = () => {
-//   // const allDoneBtn = [...document.querySelectorAll(".tasks button")];
-//   const newDoneBtn = [...document.querySelectorAll(".delete")];
-//   newDoneBtn.forEach((el) => el.addEventListener("click", closeBtn));
-//   // allDoneBtn.forEach((el) => el.addEventListener("click", closeBtn));
-// };
-
-// Seleziono task
-
-// const toggleItems = () => {
-//   const listItem = document.querySelector("ul");
-//   listItem.addEventListener(
-//     "click",
-//     function (e) {
-//       if (e.target.tagName === "LI") {
-//         e.target.classList.toggle("checked");
-//       }
-//     },
-//     false
-//   );
-// };
 
 const init = async () => {
   addTasks();
